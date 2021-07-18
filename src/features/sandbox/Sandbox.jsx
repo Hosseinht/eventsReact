e457dfcf6f735408f100b8d97a4c5af899bc4637
Ryan Taheri <sailantht@gmail.com>
2021-07-18 11:33:35 +0430

    21.Redux-SandBox,Redux Action

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import Button from "react-bootstrap/Button";
+import {DECREMENT_COUNTER, INCREMENT_COUNTER} from "./testReducer";
 
 const Sandbox = () => {
+    const dispatch = useDispatch()
     const data = useSelector(state => state.data)
     return (
         <>
             <h1>Testing 123</h1>
             <h3>Data is: {data} </h3>
+            <Button onClick={() => dispatch({type: INCREMENT_COUNTER})} variant='primary'>Increment</Button>{" "}
+            <Button onClick={() => dispatch({type: DECREMENT_COUNTER})} variant='warning'>Decrement</Button>
         </>
     );
 };
