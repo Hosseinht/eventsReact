e7f7f978638847ab96bb2c1fe4fbc861a430dd44
Ryan Taheri <sailantht@gmail.com>
2021-08-01 12:52:19 +0430

    61.Authentication-Logging

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -2,12 +2,17 @@ import {
     SIGN_IN_USER,
     SIGN_OUT_USER
 } from "./authConstants";
+import firebase from "../../app/config/firebase";
 
-export const signInUser = (payload) => {
-    return {
-        type: SIGN_IN_USER,
-        payload
-        // if the properties inside of an object matches the name of the parameter we don't need to use payload: payload
+
+export const signInUser = (creds) => async (dispatch) => {
+    try {
+        const result = await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password)
+        dispatch({type: SIGN_IN_USER, payload:result.user})
+    } catch (error) {
+        throw error
+        // because this is a form that we're using to login the user.
+        // then we're going to throw the error we get back to the form itself
     }
 };
 
